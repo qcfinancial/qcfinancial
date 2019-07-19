a2b6ace8066c53bbab6578b87a62c3f97b18adea
Alvaro Diaz Valenzuela <alvaro@efaa.cl>
2019-07-19 11:31:11 -0400

    Se eliminan directorios asociados a Visual Studio.

--- QC_Financial/Include/AuxiliaryFunctions.h
+++
@@ -1,9 +0,0 @@
-#ifndef AUXILIARYFUNCTIONS_H
-#define AUXILIARYFUNCTIONS_H
-
-#include "QCYearFraction.h"
-#include "QCWealthFactor.h"
-#include "QCInterestRate.h"
-
-
-#endif //AUXILIARYFUNCTIONS_H
--- Release/copy_pyc.bat
+++
@@ -1,1 +0,0 @@
-copy QC_DVE_PYBIND.pyd c:\Usuarios\Soporte\Anaconda2\QC_DVE_PYBIND.pyd
