0a5a0f538ea4102a97704971e5c435ba08713456
Alvaro Diaz Valenzuela <alvaro@efaa.cl>
2023-12-19 16:27:00 -0300

    Script para compilar en Windows.

---
+++ QC_DVE_CORE/compile.ps1
@@ -0,0 +1,14 @@
+pyenv global 3.8.10
+python setup.py bdist_wheel
+
+pyenv global 3.9.13
+python setup.py bdist_wheel
+
+pyenv global 3.10.11
+python setup.py bdist_wheel
+
+pyenv global 3.11.6
+python setup.py bdist_wheel
+
+pyenv global 3.12.0
+python setup.py bdist_wheel
