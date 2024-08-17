88d8ceefcb81acd380e46abde5ff49af5676bb8b
Alvaro Diaz Valenzuela <alvaro@efaa.cl>
2024-08-17 12:16:03 -0400

    La versión se mueve a 1.0.0 en vez de 0.15.0 . El cambio es necesario ya que la homologación de nombres de parámetros en los métodos de LegFactory pueden romper código existente.

--- QC_DVE_CORE/source/qcf_binder.cpp
+++ QC_DVE_CORE/source/qcf_binder.cpp
@@ -102,7 +102,7 @@ PYBIND11_MODULE(qcfinancial, m) {
 
         m.def(
                 "id",
-                []() { return "version: 0.15.0, build: 2024-08-16 18:10"; });
+                []() { return "version: 1.0.0, build: 2024-08-17 12:15"; });
 
         // QCDate
         py::class_<QCDate>(m, "QCDate", R"pbdoc(Permite representar una fecha en calendario gregoriano.)pbdoc")
